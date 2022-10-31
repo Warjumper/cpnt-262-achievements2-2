c7b8558a046eed66cdbee0f04e32d97ba77c938b
unknown <jessehorner11@gmail.com>
2022-10-31 14:10:01 -0600

    added code to css, js, html

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # cpnt-262-achievements2-2
-cpnt-262-achievements2
+### cpnt-262-achievements2 - Text area counter.
+
+#### By Jesse H
+
+### Code Attributions;
+- Great tutorial help from https://stackabuse.com/character-counter-for-text-areas-with-vanilla-javascript/
+- preventDefault taken from previous project (by me).
+
---
+++ js/character-counter.js
@@ -0,0 +1,19 @@
+//Character Counter
+const submitHandler = (event) => {
+  event.preventDefault();
+}
+
+const textArea = document.getElementById("textBio");
+
+const characterCounter = document.getElementById("textCounter");
+
+const countCharacters = () => {
+  let numOfEnteredChars = textArea.value.length;
+  let counter = numOfEnteredChars;
+  characterCounter.textContent = counter + "/400";
+};
+
+textArea.addEventListener("input", countCharacters);
+
+
+
